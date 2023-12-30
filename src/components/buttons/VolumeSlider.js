9af4ce5dff45aca529038b9f7caf5cb64ef67733
BABEKIZULU <babekizulu@gmail.com>
2023-12-30 13:00:41 +0200

    Added fonts. @TODO: Slider Functionality

--- src/components/buttons/VolumeSlider.js
+++ src/components/buttons/VolumeSlider.js
@@ -1,10 +1,26 @@
 //libs
-import React from 'react';
+import React, { useState } from 'react';
 
 const VolumeSlider = () => {
+  //state management
+  const [mouseX, setMouseX] = useState(0);
+  const [sliderX, setSliderX] = useState(0);
+  //handlers
+  const onMouseMoveHandler = (x) => {
+    setMouseX(x);
+  };
+  const onSliderDrag = () => {
+    //
+  };
   return (
-    <div className='volume-slider-container'>
-      <div className='slider-knob'></div>
+    <div
+      className='volume-slider-container'
+      onMouseMove={(e) => onMouseMoveHandler(e.clientX)}
+    >
+      <div
+        className='slider-knob'
+        style={`transform: TranslateX(${sliderX})`}
+      ></div>
       <div className='slide-meter'></div>
     </div>
   );
